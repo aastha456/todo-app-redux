8b343cc6542f9285dd64c10fe9143973ff212a8e
aastha456 <tamrakaraastha7@gmail.com>
2026-03-21 12:59:45 +0545

    Add edit, filter functionality in the app

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,51 +1,116 @@
-import { addTodo, toggleTodo, deleteTodo } from "../store/slices/todoSlice"
-import {useAppDispatch, useAppSelector} from "../hooks/hooks"
-import { useState, useEffect } from "react"
-import './Todo.css'
+import {addTodo,toggleTodo,deleteTodo,editTodo} from "../store/slices/todoSlice";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import { useState, useEffect } from "react";
+import "./Todo.css";
 
 const Todo = () => {
   const todos = useAppSelector((state) => state.todo.todos);
   const dispatch = useAppDispatch();
   const [text, setText] = useState("");
+  const [editingId, setEditingId] = useState<string | null>(null);
+  const [editText, setEditText] = useState("");
+  const [filter, setFilter] = useState<"all" | "active" | "completed">("all");
+  
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }, [todos])
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const handleAdd = () => {
     if (!text.trim()) return;
-    dispatch(addTodo({text}));
+    dispatch(addTodo({ text }));
     setText("");
-  }
+  };
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === "active") return !todo.completed;  
+    if (filter === "completed") return todo.completed;
+    return true;
+  })
   return (
     <div className="todo-wrapper">
       <div className="todo-card">
         <h2>Your todo list</h2>
         <div className="todo-row">
-          <input type="text" placeholder="Add new task" value={text} onChange={(e) => setText(e.target.value)} />
+          <input
+            type="text"
+            placeholder="Add new task"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
           <button onClick={handleAdd}>Add</button>
-
+        </div>
+        <div className="todo-filters">
+          <button
+            className={`filter ${filter === "all" ? "active" : ""}`}
+            onClick={() => setFilter("all")}>All</button>
+          <button className={`filter ${filter === "active" ? "active" : ""}`}
+          onClick={() => setFilter("active")}>Active</button>
+           <button className={`filter ${filter === "completed" ? "completed" : ""}`}
+          onClick={() => setFilter("completed")}>Completed</button>
         </div>
         <ul className="todo-list">
-           {todos.map(todo => (
+          {filteredTodos.map((todo) => (
             <li key={todo.id} className="todo-item">
               <div className="todo-left">
-                <input type="checkbox" checked={todo.completed} onChange={() =>dispatch(toggleTodo({id: todo.id}))}/>
-                  <span className={`todo-text ${todo.completed ? "completed": ""}`}>{todo.text}</span>    
-                  <button className="todo-btn" onClick={() => dispatch(deleteTodo({id: todo.id}))}>x</button>  
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => dispatch(toggleTodo({ id: todo.id }))}
+                />
+
+                {editingId === todo.id ? (
+                  <>
+                    <input
+                      type="text"
+                      value={editText}
+                      onChange={(e) => setEditText(e.target.value)}
+                      className="edit-input"
+                    />
+                  </>
+                ) : (
+                  <>
+                    <span
+                      className={`todo-text ${todo.completed ? "completed" : ""}`}
+                    >
+                      {todo.text}
+                    </span>
+                  </>
+                )}
               </div>
-                
+
+              <div className="todo-actions">
+                {editingId === todo.id ? (
+                  <>
+                  <button className="todo-btn" onClick={() => {
+                    if (editText.trim()){
+                      dispatch(editTodo({ id: todo.id, text: editText }));
+                    }
+                    
+                    setEditingId(null);
+                  }}>Save</button>
+
+                  <button className="todo-btn" onClick={() => setEditingId(null)}>Cancel</button>
+                  </>
+                ) : (
+                  <>
+                  <button className="todo-btn" onClick={() => {
+                    setEditingId(todo.id);
+                    setEditText(todo.text);
+                  }}>Edit</button> 
+                  <button className="todo-btn" onClick={() => dispatch(deleteTodo({ id: todo.id }))}>X</button>
+
+                  </>
+                )
+                }
+              </div>
             </li>
-            ))}
+          ))}
         </ul>
         <div className="todo-remaining">
-          Your todo remaining: {todos.filter(t => !t.completed).length}
+          Your todo remaining: {todos.filter((t) => !t.completed).length}
         </div>
-
       </div>
-        
-      
     </div>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
--- src/store/slices/todoSlice.ts
+++ src/store/slices/todoSlice.ts
@@ -1,26 +1,29 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
 
+// define the structure of todo app
 interface Todo {
     id: string,
     text: string,
     completed: boolean
 }
-
+// define the structure of todo slice state
 interface TodoState {
     todos: Todo[]
 }
-
+// loads todo from the localstorage 
+// this function tries to read the saved data that were stored in the local storage 
+// if there are no saved data then it return empty array 
 const loadFromLocalStorage = (): Todo[] => {
     try{
         const todos = localStorage.getItem("todos");
         return todos ? JSON.parse(todos) : [];
-
     } catch (e) {
         console.log("Failed to load the todos", e)
         return [];
     }
 }
-
+//Saves the todo app in the localstorage
+// it will save the data as a JSON string 
 const saveToLocalStorage = (todos: Todo[]) => {
     try {
         localStorage.setItem("todos", JSON.stringify(todos));
@@ -29,24 +32,28 @@ const saveToLocalStorage = (todos: Todo[]) => {
     }
 };
 
+//initial state of the app start 
+// it loads todo from the localStorage so that the data persists across the page reloads 
 const todoInitialState: TodoState = {
     todos: loadFromLocalStorage()
 }
-
+// create the slice 
 const todoSlice = createSlice({
     name: "todo",
     initialState: todoInitialState,
     reducers: {
+        // add the todo
         addTodo: (state, action: PayloadAction<{text: string}>) => {
             const newTodo: Todo = {
-                id: Date.now().toString(),
-                text: action.payload.text,
+                id: Date.now().toString(),// generate the unique ID
+                text: action.payload.text, // displays as text 
                 completed: false
             }
             state.todos.push(newTodo);
             saveToLocalStorage(state.todos);
         },
         toggleTodo: (state, action: PayloadAction<{id: string}>) => {
+            // finds teh todo app which mathed the ID
             const todo = state.todos.find(t => t.id === action.payload.id);
             if(todo){
                 todo.completed = !todo.completed;
@@ -54,12 +61,22 @@ const todoSlice = createSlice({
             saveToLocalStorage(state.todos);
         },
         deleteTodo: (state, action: PayloadAction<{id: string}>) => {
+            // filter out the todo app with the matching ID
+            // keep all tht todo app wehre the ID is not matched 
             state.todos = state.todos.filter(t => t.id !== action.payload.id);
             saveToLocalStorage(state.todos);
+        },
+        editTodo: (state, action: PayloadAction<{id: string, text: string}>) => {
+            const todo =  state.todos.find(t => t.id === action.payload.id);
+            if (todo){
+                todo.text = action.payload.text;
+            }
+            saveToLocalStorage(state.todos);
+
         }
 
     } 
 });
 
-export const { addTodo, toggleTodo, deleteTodo} = todoSlice.actions;
+export const { addTodo, toggleTodo, deleteTodo, editTodo} = todoSlice.actions;
 export default todoSlice.reducer;
